89d87702c42272fa9ddf18d3f37fc98a455df374
Ilya <shcherbakov.illya@list.ru>
2025-08-04 05:01:55 +0400

    finished

---
+++ build/js/entry/hoisted-7a16b8af.js
@@ -0,0 +1,3 @@
+import { O as Oe } from "./hoisted-d4514765.js";
+
+Oe.bind(".fanGallery", {});
---
+++ build/js/module.js
@@ -0,0 +1,3 @@
+import "./entry/hoisted-7a16b8af.js";
+import "./entry/hoisted-960c040f.js";
+import "./entry/hoisted-d4514765.js";
